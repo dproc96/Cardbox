7f7e1acc4e165aeb51140841c1a212d0a54cf9fc
Dan Proctor <dproc96@gmail.com>
2020-02-12 11:06:47 -0500

    wrote tests for User model

--- models/User.js
+++ models/User.js
@@ -1,13 +1,20 @@
+/* eslint-disable no-useless-escape */
 const mongoose = require("mongoose");
+const uniqueValidator = require('mongoose-unique-validator');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const emailValidator = function(value) {
+    return value.match(/[\w-\.]+@\w+\.\w+/);
+}
 
 const UserSchema = new Schema({
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        validate: [emailValidator, "email field must be an email"]
     },
     password: {
         type: String,
@@ -15,7 +22,8 @@ const UserSchema = new Schema({
     },
     username: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     tokens: [{
         token: {
@@ -33,30 +41,30 @@ const UserSchema = new Schema({
     }]
 })
 
+UserSchema.plugin(uniqueValidator);
+
 UserSchema.methods.generateAuthToken = async function () {
     const user = this;
     const token = jwt.sign({ _id: user._id.toString() }, 'bestteam');
     user.tokens = user.tokens.concat({ token })
     await user.save()
     return token
-
 }
 
-UserSchema.statics.findByCredentials = async (email, password) => {
-    const user = await User.findOne({ email })
-
+UserSchema.statics.logIn = async function(email, password) {
+    const user = await this.findOne({ email: email })
     if (!user) {
-        throw new Error('Unable to login')
+        throw new Error("No user found")
     }
     const isMatch = await bcrypt.compareSync(password, user.password)
-
-    if (!isMatch) {
-        throw new Error('Unable to login')
+    if (isMatch) {
+        const token = await user.generateAuthToken()
+        return token;
     }
-    // if the username and password match returns the user
-    return user
+    else {
+        throw new Error("Password does not match")
+    }
 }
-
 
 //Hash the plain text password
 UserSchema.pre('save', async function (next) {
--- test/database.test.js
+++ test/database.test.js
@@ -1,6 +1,7 @@
 const {assert} = require("chai");
 const {User, Note, Category} = require("../models");
 const {connect, disconnect} = require("../utils/database");
+const bcrypt = require("bcryptjs")
 
 describe("Database", () => {
     beforeEach(connect);
@@ -49,6 +50,164 @@ describe("Database", () => {
                 });
                 note.alterHtml("new text");
                 assert.strictEqual(note.html, "new text");
+            })
+        })
+    })
+    describe("User", () => {
+        describe("#email", () => {
+            it("is a string", () => {
+                const user = new User({
+                    email: "test@test.com"
+                })
+                assert.strictEqual(user.email, "test@test.com")
+            })
+            it("should be invalid with an invalid email", () => {
+                const user = new User({
+                    email: "test",
+                    password: "test",
+                    username: "test"
+                })
+                user.validateSync();
+                assert.ok(user.errors, "model should be invalid");
+                assert.equal(user.errors.email.message, "email field must be an email");
+            })
+            it("should be valid with an valid email", () => {
+                const user = new User({
+                    email: "test@test.com",
+                    password: "test",
+                    username: "test"
+                })
+                user.validateSync();
+                assert.isUndefined(user.errors, "model should be valid");
+            })
+            it("must be unique", async () => {
+                const user = new User({
+                    email: "test@test.com",
+                    password: "test",
+                    username: "test"
+                });
+                await user.save();
+                const userDuplicate = new User({
+                    email: "test@test.com",
+                    password: "test",
+                    username: "test2"
+                });
+                userDuplicate.save(error => {
+                    assert.ok(error)
+                    assert.equal(error.errors.email.message, "Error, expected `email` to be unique. Value: `test@test.com`");
+                })
+            })
+        })
+        describe("#password", () => {
+            it("is a string", () => {
+                const user = new User({
+                    password: "test"
+                })
+                assert.strictEqual(user.password, "test")
+            })
+            it("is hashed on save", async () => {
+                const password = "test"
+                const user = new User({
+                    email: "test@test.com",
+                    password: password,
+                    username: "test"
+                });
+                await user.save();
+                const isHashed = bcrypt.compareSync(password, user.password);
+                assert.ok(isHashed)
+            })
+        })
+        describe("#username", () => {
+            it("is a string", () => {
+                const user = new User({
+                    username: "test"
+                })
+                assert.strictEqual(user.username, "test")
+            })
+            it("must be unique", async () => {
+                const user = new User({
+                    email: "test@test.com",
+                    password: "test",
+                    username: "test"
+                });
+                await user.save();
+                const userDuplicate = new User({
+                    email: "test2@test.com",
+                    password: "test",
+                    username: "test"
+                });
+                userDuplicate.save(error => {
+                    assert.ok(error)
+                    assert.equal(error.errors.username.message, "Error, expected `username` to be unique. Value: `test`");
+                })
+            })
+        })
+        describe("#generateAuthToken", () => {
+            it("generates a token", async () => {
+                const user = new User({
+                    email: "test@test.com",
+                    password: "test",
+                    username: "test"
+                });
+                await user.save();
+                const token = await user.generateAuthToken();
+                assert.isString(token);
+            })
+            it("saves the token to the db", async () => {
+                const user = new User({
+                    email: "test@test.com",
+                    password: "test",
+                    username: "test"
+                });
+                await user.save();
+                const token = await user.generateAuthToken();
+                assert.strictEqual(user.tokens[0].token, token)
+            })
+        })
+        describe(".logIn", () => {
+            it("returns a token if successful", async () => {
+                const email = "test@test.com";
+                const password = "test";
+                const user = new User({
+                    email: email,
+                    password: password,
+                    username: "test"
+                });
+                await user.save();
+                const token = await User.logIn(email, password);
+                assert.isString(token);
+            })
+            it("throws an error if email doesn't match", async () => {
+                const email = "test@test.com";
+                const password = "test";
+                const user = new User({
+                    email: email,
+                    password: password,
+                    username: "test"
+                });
+                await user.save();
+                try {
+                    const token = await User.logIn("test@tes.com", password);
+                }
+                catch (error) {
+                    assert.strictEqual(error.message, "No user found")
+                }
+            })
+            it("throws an error if password doesn't match", async () => {
+                const email = "test@test.com";
+                const password = "test";
+                const user = new User({
+                    email: email,
+                    password: password,
+                    username: "test"
+                });
+                await user.save();
+                try {
+                    const token = await User.logIn(email, "tes");
+                }
+                catch (error) {
+                    assert.strictEqual(error.message, "Password does not match")
+                }
             })
         })
     })
