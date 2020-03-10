9c27b69da08f30b87b54091fe04b17d4f8d8bc37
Dan Proctor <dproc96@gmail.com>
2020-03-09 21:22:52 -0400

    added logic to set up resizing

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -13,22 +13,52 @@ class Card extends React.Component {
             dragging: false
         }
     }
+    checkResizing(e) {
+        const distanceX = e.pageX - this.state.x
+        const distanceY = e.pageY - this.state.y
+        const resizingNwse = (distanceX <= 5 && distanceY <= 5) || (this.state.width - distanceX <= 5 && this.state.height - distanceY <= 5)
+        const resizingNesw = (this.state.width - distanceX <= 5 && distanceY <= 5) || (distanceX <= 5 && this.state.height - distanceY <= 5)
+        if (resizingNwse) {
+            this.setState({ resizeHover: "nwse" })
+        }
+        else if (resizingNesw) {
+            this.setState({ resizeHover: "nesw" })
+        }
+        else {
+            this.setState({ resizeHover: false })
+        }
+    }
     handleMouseMove(e) {
         if (this.state.dragging) {
             console.log(this.state.offsetY)
             console.log(e.pageY)
             this.setState({ x: e.pageX - this.state.offsetX, y: e.pageY - this.state.offsetY });
         }
+        this.checkResizing(e)
     }
     handleMouseDown(e) {
-        this.setState({ 
-            offsetX: e.pageX - this.state.x,
-            offsetY: e.pageY - this.state.y,
-            dragging: true 
-        })
+        if (this.state.resizeHover) {
+            console.log("resizing")
+        }
+        else {
+            this.setState({ 
+                offsetX: e.pageX - this.state.x,
+                offsetY: e.pageY - this.state.y,
+                dragging: true 
+            })
+        }
     }
     handleMouseUp() {
         this.setState({ dragging: false })
+    }
+    handleMouseEnter(e) {
+        this.checkResizing(e)
+    }
+    handleMouseLeave() {
+        this.setState({ 
+            resizeHover: false,
+            dragging: false
+        })
     }
     render() {
         const style = {
@@ -41,11 +71,12 @@ class Card extends React.Component {
             top: this.state.y,
             left: this.state.x,
             boxShadow: "2px 2px 2px 2px rgba(0,0,0,0.3)",
-            userSelect: this.state.dragging ? "none" : "auto",
-            resize: "both"
+            userSelect: (this.state.dragging || this.state.resizeHover) ? "none" : "auto",
+            resize: "both",
+            cursor: this.state.resizeHover ? `${this.state.resizeHover}-resize` : "auto"
         }
         return (
-            <div onMouseDown={this.handleMouseDown.bind(this)} onMouseUp={this.handleMouseUp.bind(this)} onMouseMove={this.handleMouseMove.bind(this)} style={style}>
+            <div onMouseDown={this.handleMouseDown.bind(this)} onMouseUp={this.handleMouseUp.bind(this)} onMouseMove={this.handleMouseMove.bind(this)} onMouseEnter={this.handleMouseEnter.bind(this)} onMouseLeave={this.handleMouseLeave.bind(this)} style={style}>
                 <h3>{this.state.title}</h3>
                 <div>
                     {this.state.html}
